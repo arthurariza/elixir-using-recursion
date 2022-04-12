54fe6d654033061ed70226d660c027e03a575951
arthurariza <tucaariza@gmail.com>
2022-04-11 22:21:04 -0300

    feat: list_length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  # Base case
+  defp count([], acc), do: acc
+
+  defp count([_head | tail], acc) do
+    acc = 1 + acc
+    count(tail, acc)
   end
 end
